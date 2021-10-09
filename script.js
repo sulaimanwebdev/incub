78db53aaae99a4573f1c718785135ce61bbe2621
Muhammad Sulaiman <74793541+sulaimanwebdev@users.noreply.github.com>
2021-10-09 13:58:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let btnDrop = document.querySelector('.btnDrop');
+let dropdown = document.querySelector('.dropdown');
+
+btnDrop.addEventListener('click', () =>{
+dropdown.classList.toggle('open')
+});
+
+
+  
